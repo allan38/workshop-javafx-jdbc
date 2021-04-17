c18fecc5560c084b38dfd303aed3822c68dfb221
allan38 <allanpeixoto38@gmail.com>
2021-04-17 18:23:08 -0300

    Create Components

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 
@@ -14,8 +15,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) { 
 		try { 
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml")); 
-			Parent parent = loader.load(); 
-			Scene mainScene = new Scene(parent); 
+			ScrollPane sp = loader.load(); 
+			Scene mainScene = new Scene(sp); 
+			sp.setFitToHeight(true);
+			sp.setFitToWidth(true);
 			primaryStage.setScene(mainScene); 
 			primaryStage.setTitle("Sample JavaFX application"); 
 			primaryStage.show(); 
