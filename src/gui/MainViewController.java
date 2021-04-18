0d5b36b6c4cdcc36cc36f4ec3fc05657bcd1a2d8
allan38 <allanpeixoto38@gmail.com>
2021-04-18 02:48:18 -0300

    DepartmentService

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable {
 
@@ -24,37 +25,59 @@ public class MainViewController implements Initializable {
 	private MenuItem menuItemDepartment;
 	@FXML
 	private MenuItem menuItemAbout;
-	
+
 	public void onItemSellerAction () {
 		System.out.println("Seller");
 	}
-	
+
 	public void onItemDepartmentAction () {
-		loadView("/gui/DepartmentList.fxml");
+		loadView2("/gui/DepartmentList.fxml");
 	}
-	
+
 	public void onItemAboutAction () {
 		loadView("/gui/About.fxml");
 	}
-	
+
 	public void loadView (String absolutName) {
-		
+
 		try {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutName)); 
-		VBox vbox = loader.load();
-		Scene mainScene = Main.getMainScene();
-		VBox vboxmain = (VBox)((ScrollPane) mainScene.getRoot()).getContent();
-		
-		Node menuMain = vboxmain.getChildren().get(0);
-		vboxmain.getChildren().clear();
-		vboxmain.getChildren().add(menuMain);
-		vboxmain.getChildren().addAll(vbox.getChildren());
-		
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutName)); 
+			VBox vbox = loader.load();
+			Scene mainScene = Main.getMainScene();
+			VBox vboxmain = (VBox)((ScrollPane) mainScene.getRoot()).getContent();
+
+			Node menuMain = vboxmain.getChildren().get(0);
+			vboxmain.getChildren().clear();
+			vboxmain.getChildren().add(menuMain);
+			vboxmain.getChildren().addAll(vbox.getChildren());
+
 		}catch(IOException e) {
 			Alerts.showAlert(e.getMessage(), null, "Não foi possível carregar a tela.", AlertType.ERROR);
 		}
 	}
-	
+
+	public void loadView2 (String absolutName) {
+
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absolutName)); 
+			VBox vbox = loader.load();
+			Scene mainScene = Main.getMainScene();
+			VBox vboxmain = (VBox)((ScrollPane) mainScene.getRoot()).getContent();
+
+			Node menuMain = vboxmain.getChildren().get(0);
+			vboxmain.getChildren().clear();
+			vboxmain.getChildren().add(menuMain);
+			vboxmain.getChildren().addAll(vbox.getChildren());
+			
+			DepartmentListController controller = loader.getController();
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+
+		}catch(IOException e) {
+			Alerts.showAlert(e.getMessage(), null, "Não foi possível carregar a tela.", AlertType.ERROR);
+		}
+	}
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		// TODO Auto-generated method stub
